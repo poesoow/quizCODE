937319cd554a0418440a10af7a24ec994c4be643
= <ws71144612@daum.net>
2023-05-06 11:42:17 +0900

    testview 완료

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full flex items-center justify-center flex-wrap h-full">
     <div class="mx-auto w-10/12 lg:w-7/12 flex items-center flex-wrap">
-      <button @click="current = 1">다시 시작</button>
+      <button @click="current = 0">다시 시작</button>
       {{ current }} / {{ selectCount }}
       <!-- 문제진행률(프로그레스 바) -->
       <div class="h-5 bg-gray-300 basis-full relative rounded-lg">
@@ -27,25 +27,65 @@
       <div
         v-if="current < Number(selectCount)"
         class="bg-white rounded-lg my-10 p-10 basis-full question">
-        <div>{{ current }}번 문제</div>
+        <div>{{ current + 1 }}번 문제</div>
         <p class="text-base sm:text-xl">{{ selectQuizs[current].question }}</p>
         <ul
           class="mt-5 flex flex-wrap justify-between" >
           <li
           v-for="(answer, key, index) in selectQuizs[current].answers" :key="key"
-          @click="current++; selectValue(answer as string)"
+          @click="selectValue(answer as string, key, current)"
           class="cursor-pointer font-bold basis-full lg:basis-[49%] rounded-lg mb-3 border p-3 text-sm hover:border-blue-500 hover:text-blue-500 duration-500">
           <template v-if="answer">
-              <span>보기 {{ index + 1 }}</span>
+              <span>보기{{ index + 1 }}</span>
               <span class="break-all text-center pl-2">{{ answer }}</span>
             </template>
             </li>
         </ul>
+        <!--
+          선택을 나중에 다시 할 수 있도록 하려면????
+          <button @click="current--">다음문제</button>
+          <button @click="current++">다음문제</button>
+        -->
       </div>
-      {{ userSelects }}
-      <br>
-      {{ resultQuiz }}
-      <button @click="current++">다음문제</button>
+      <!-- 결과 영역 -->
+      <div v-else class="w-full ">
+        <div class="mx-auto flex items-center flex-wrap">
+          <div class="text-xl">
+            <p>정답개수 : {{ correctCount() }}개 {{ resultScore() }}점</p>
+            <button
+              @click="OpenAnswernote"
+              v-if="isAnswerNote === false"
+              class="text-base">틀린문향 확인하기</button>
+          </div>
+          <div
+            v-if="isAnswerNote === true"
+            class="bg-white rounded-lg my-10 p-10 basis-full question">
+            <div>{{ answerCurrent + 1 }}번 문제</div>
+            <p class="text-base sm:text-xl">{{ answerNote()[answerCurrent].question }}</p>
+            <ul
+              class="mt-5 flex flex-wrap justify-between" >
+              <li
+              v-for="(answer, key, index) in answerNote()[answerCurrent].answers" :key="key"
+              class="font-bold basis-full lg:basis-[49%] rounded-lg mb-3 border p-3 text-sm duration-500">
+              <template v-if="answer">
+                <span>보기{{ index + 1 }}</span>
+
+                <template v-for="(correctAnswer, key2, index2) in answerNote()[answerCurrent]['correct_answers']" :key="key2">
+                  <span
+                    v-if="index == index2"
+                    :class="[correctAnswer == 'true' && 'text-red-500 ', answerNote()[answerCurrent].selectValue == answer && 'text-indigo-500'] "
+                    class="break-all text-center px-2">
+                    {{ answer }}
+                    </span>
+                </template>
+
+              </template>
+                </li>
+            </ul>
+            <button v-if="answerCurrent < answerNote().length - 1" @click="answerCurrent++">다음문제 확인</button>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 
@@ -73,7 +113,7 @@ interface QuizType {
 
 interface UserResult extends QuizType {
   selectValue?: string;
-  result?: boolean;
+  result?: string | boolean;
 }
 
 export default defineComponent({
@@ -81,13 +121,16 @@ export default defineComponent({
   data() {
     return {
       userName: this.$route.query.userName,
+      selectRandom: this.$route.query.selectRandom,
       selectDiffculty: this.$route.query.selectDiffculty,
       selectCate: this.$route.query.selectCate,
       limitCount: this.$route.query.limitCount,
       selectCount: this.$route.query.selectCount,
       limitQuizs: this.$store.state.limitQuizs,
-      current: 1,
-      userSelects: [] as string[]
+      current: 0,
+      userSelects: [] as string[],
+      isAnswerNote: false,
+      answerCurrent: 0,
     }
   },
   computed: {
@@ -96,25 +139,53 @@ export default defineComponent({
     },
     // 퀴즈 문제 랜덤 및 선택한 개수만큼 선택
     selectQuizs(): QuizType[] {
-      return this.limitQuizs
+      if(this.selectRandom == "1"){
+        return this.limitQuizs.slice().sort(() => Math.random() - 0.5).slice(0, Number(this.selectCount))
+      } else {
+        return this.limitQuizs.slice().sort().slice(0, Number(this.selectCount))
+      }
     },
-    resultScore() :number {
-      return Math.floor(1)
-    },
+    // 사용자 선택항목을 넣기
     resultQuiz(): UserResult[] {
-      return this.selectQuizs.slice()
-    }
+      return Array.from(this.selectQuizs, (quiz) => ({
+        ...quiz,
+        selectValue: '',
+        result: ''
+      }))
+    },
+
   },
   methods: {
-    // selectValue(e :undefined):boolean{ return false},
-    selectValue(value :string){
+    selectValue(value :string, key: string , index: number) :void {
+      const correctAnswers = this.resultQuiz[index]['correct_answers']
+      this.resultQuiz[index].selectValue = value
+      this.resultQuiz[index].result = correctAnswers[`${key}_correct`]
+      // 굳이 안넣어도 되긴하는데...
       this.userSelects.push(value)
+      // 다음문제를 위해서
+      this.current++;
+    },
+    // 정답 개수
+    correctCount(): number {
+      return this.resultQuiz.filter((quiz) => {
+        return quiz.result === "true"
+      }).length
+    },
+    // 결과 점수 백점 만점
+    resultScore(): number {
+      const count = this.correctCount()
+      const total = this.resultQuiz.length
+      return Math.floor((count / total) * 100)
+    },
+    OpenAnswernote() {
+      this.isAnswerNote = true
+    },
+    // 사용자 틀린 문제
+    answerNote(): UserResult[] {
+      return this.resultQuiz.filter((quiz) => {
+        return quiz.result == "false"
+      })
     }
-  },
-  created() {
-    console.log(1)
-
-
   },
 })
 </script>
